4fca13e8d7285387e110716bb3a36ad87a623eff
Naor Ye <naorye@gmail.com>
2014-01-27 00:00:06 +0200

    - Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-angular-ny-logger
-=================
+#angular-ny-logger
 
-AngularJS Logger service provider
+> AngularJS Logger service provider
+
+## Getting Started
+
+This is a AngularJS Logger service provider plugin. After integrating this service with your application you will be able to get informative logs as well as turn them off in production. 
+
+More information can be found here: <a href="http://www.webdeveasy.com/logger-for-angularjs" target="_blank">http://www.webdeveasy.com/logger-for-angularjs</a>
+
+## Usage
+
+1. Include angular-ny-logger.js in your JavaScript files.
+2. Add `ny.logger` module as a dependency to your module:
+    ```
+    angular.module('YourModule', ['ny.logger'])
+    ```
+3. Make a configuration block that turns on or off logging:
+    ```
+	module.config(['LoggerProvider', function(LoggerProvider) {
+		// We don't want the Logger service to be enabled in production
+   		LoggerProvider.enabled(!isProduction);
+   	}]);
+    ```
+4. Start logging like a pro with informative logs:
+    ```
+	.controller('ExampleController', ['Logger', function(Logger) {
+		var logger = Logger.getInstance('ExampleController');
+		logger.log('This is a log');
+		logger.warn('warn', 'This is a warn');
+		logger.error('This is a {0} error! {1}', [ 'big', 'just kidding' ]);
+		logger.debug('debug', 'This is a debug for line {0}', [ 8 ]);
+  	}]);
+	```
+* * *
+
+Copyright (c) 2014 naorye
---
+++ angular-ny-logger.js
@@ -0,0 +1,76 @@
+angular.module('ny.logger', []).provider('Logger', [function () {
+    var isEnabled = true;
+    this.enabled = function(_isEnabled) {
+        isEnabled = !!_isEnabled;
+    };
+    this.$get = ['$log', function($log) {
+        var Logger = function(context) {
+            this.context = context;
+        };
+        Logger.getInstance = function(context) {
+            return new Logger(context);
+        };
+        Logger.supplant = function(str, o) {
+            return str.replace(
+                /\{([^{}]*)\}/g,
+                function (a, b) {
+                    var r = o[b];
+                    return typeof r === 'string' || typeof r === 'number' ? r : a;
+                }
+            );
+        };
+        Logger.getFormattedTimestamp = function(date) {
+           return Logger.supplant('{0}:{1}:{2}:{3}', [
+                date.getHours(),
+                date.getMinutes(),
+                date.getSeconds(),
+                date.getMilliseconds()
+            ]);
+        };
+        Logger.prototype = {
+            _log: function(originalFn, args) {
+                if (!isEnabled) {
+                    return;
+                }
+                
+                var now  = Logger.getFormattedTimestamp(new Date());
+                var message = '', supplantData = [];
+                switch (args.length) {
+                    case 1:
+                        message = Logger.supplant("{0} - {1}: {2}", [ now, this.context, args[0] ]);
+                        break;
+                    case 3:
+                        supplantData = args[2];
+                        message = Logger.supplant("{0} - {1}::{2}(\'{3}\')", [ now, this.context, args[0], args[1] ]);
+                        break;
+                    case 2:
+                        if (typeof args[1] === 'string') {
+                            message = Logger.supplant("{0} - {1}::{2}(\'{3}\')", [ now, this.context, args[0], args[1] ]);
+                        } else {
+                            supplantData = args[1];
+                            message = Logger.supplant("{0} - {1}: {2}", [ now, this.context, args[0] ]);
+                        }
+                        break;
+                }
+
+                $log[originalFn].call(null, Logger.supplant(message, supplantData));
+            },
+            log: function() {
+                this._log('log', arguments);
+            },
+            info: function() {
+                this._log('info', arguments);
+            },
+            warn: function() {
+                this._log('warn', arguments);
+            },
+            debug: function() {
+                this._log('debug', arguments);
+            },
+            error: function() {
+                this._log('error', arguments);
+            }
+        };
+        return Logger;
+    }];
+}]);
